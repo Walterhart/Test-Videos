467a7d1312b7b2d235229a6a52232702bcdb89c4
Walterhart <70658679+Walterhart@users.noreply.github.com>
2022-10-17 05:48:24 -0400

    Add navbar and homepage component. Also add basic css color

---
+++ amnis-video/src/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (  
+        <div className="HomePage">
+            <h3>HomePage </h3>
+        </div>
+    );
+}
+ 
+export default HomePage;
---
+++ amnis-video/src/Navbar.js
@@ -0,0 +1,14 @@
+// Navbar component 
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1> navbar </h1>
+            <div className="links">
+                <a href = "/"> Home</a>
+                <a href = "/about-us"> About us</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
