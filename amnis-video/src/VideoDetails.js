c6f1dc79b91765a49adae8192eb7a0e8cfd0d65a
Walterhart <70658679+Walterhart@users.noreply.github.com>
2022-10-18 17:34:54 -0400

    Add route Parameters and display video details base on id parameters

--- amnis-video/src/VideoDetails.js
+++ amnis-video/src/VideoDetails.js
@@ -1,12 +1,25 @@
 //Componet for video details
 //useParams allowed passing route parameters
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch';
 
 const VideoDetails = () => {
     const { id }  = useParams()  
+    const { data: video, isPending, error } = useFetch('http://localhost:8000/videos/'+ id);
+
      return ( 
         <div className="videoDetails">
-        <h2>Video Detail - { id }</h2>
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error } </div>}
+            {video &&(<article>
+                <h2> {video.title}</h2>
+                <h3>Directed by: </h3>
+                <p> { video.directors }</p>
+                <h3>Description: </h3>
+                <p> { video.description }</p>
+                <h3>Avaible platforms:</h3>
+                <p> {video.source}</p>
+            </article>)}
         </div>
      );
 }
